79ca9335ef229d7a840999d3352198c18f0855ae
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-21 07:36:11 +0200

    feat(products): add sorting by query param
    - Handle 'sort' query parameter
    - Enable sorting by multiple fields
    - Default sort products by creation date

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,7 @@ const getAllProductsStatic = (req, res) => {
   res.status(200).json("sucess ");
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -14,7 +14,14 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $option: "i" };
   }
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
 
   res.status(200).json({ products });
 };
